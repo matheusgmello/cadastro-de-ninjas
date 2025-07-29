bc19a9ace7cef3fc4e515536bf9e651cdecad9fb
Matheus Mello <matheusgmello1@gmail.com>
2025-07-29 19:27:15 -0300

    feat: Criar as rotas do controller com mock

--- src/main/java/dev/matheus/CadastroDeNinjas/Ninjas/Controllers/NinjaController.java
+++ src/main/java/dev/matheus/CadastroDeNinjas/Ninjas/Controllers/NinjaController.java
@@ -1,9 +1,7 @@
 package dev.matheus.CadastroDeNinjas.Ninjas.Controllers;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,5 +10,36 @@ public class NinjaController {
     @GetMapping("/boasVindas")
     public String boasVindas() {
         return "Bem-vindo ao Cadastro de Ninjas!";
+    }
+
+    // Adicionar Ninja
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja cadastrado com sucesso!";
+    }
+
+    // Mostrar todos os Ninjas
+    @GetMapping("/todos")
+    public String mostrarNinja() {
+        return "Lista de todos os ninjas!";
+    }
+
+    // Procurar Ninja por ID
+    @GetMapping("/todos/{id}")
+    public String mostrarNinjaPorId() {
+        return "Lista de todos os ninjas!";
+    }
+
+
+    // Alterar dados dos ninjas
+    @PutMapping("/alterar/{id}")
+    public String alterarNinjaPorId() {
+        return "Dados alterados com sucesso!";
+    }
+
+    // Deletar Ninja por ID
+    @DeleteMapping("/deletar/{id}")
+    public String deletarNinjaPorId() {
+        return "Ninja deletado com sucesso!";
     }
 }
