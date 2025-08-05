75de544c4880af8694e2b25c88e9e9bc1db7ac17
Matheus Mello <matheusgmello1@gmail.com>
2025-08-05 17:02:07 -0300

    chore: Inclui a versao do DTO pela entidadade e mapeia manualmente no NinjaMapper e MissoesMapper os caminhos entidade x dto

--- src/main/java/dev/matheus/CadastroDeNinjas/Missoes/Mappers/MissoesMapper.java
+++ src/main/java/dev/matheus/CadastroDeNinjas/Missoes/Mappers/MissoesMapper.java
@@ -1,4 +1,29 @@
 package dev.matheus.CadastroDeNinjas.Missoes.Mappers;
 
+import dev.matheus.CadastroDeNinjas.Missoes.DTOs.MissoesDTO;
+import dev.matheus.CadastroDeNinjas.Missoes.Models.MissoesModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissoesMapper {
+
+    public MissoesModel map(MissoesDTO missoesDTO){
+        MissoesModel missoesModel = new MissoesModel();
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNome(missoesDTO.getNome());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+        missoesModel.setNinjas(missoesDTO.getNinjas());
+
+        return missoesModel;
+    }
+
+    public MissoesDTO map(MissoesModel missoesModel){
+        MissoesDTO missoesDTO = new MissoesDTO();
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setNome(missoesModel.getNome());
+        missoesDTO.setDificuldade(missoesModel.getDificuldade());
+        missoesDTO.setNinjas(missoesModel.getNinjas());
+
+        return missoesDTO;
+    }
 }
